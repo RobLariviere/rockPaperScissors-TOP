0802b8c757870c40f3136a46477598934dc3ec46
RobLariviere <roblariviere18@gmail.com>
2021-12-14 16:49:27 -0500

    Got the first round working, tabulated round 1 score

--- script2.js
+++ script2.js
@@ -1,15 +1,16 @@
 let computerPlay;
-let userPlay = prompt("Rock, Paper, or Scissors?");
 let youWin  = "You Win";
 let youLose = "You Lose";
 let winStatus;
 let youTie = "You Tie, play again";
 let userSelection;
 let computerSelection;
+let userScore = 0
+let computerScore = 0
 
 
 
-//Define Computer's Play
+//Define Computer's Play, Display it
 computerPlay = Math.floor(Math.random() * 3) + 1;
 
 if (computerPlay == 1) {
@@ -19,21 +20,27 @@ if (computerPlay == 1) {
   } else {
     computerSelection = "Scissors";
   }
-//Define User's Play
-  if (userPlay.toLowerCase() == "rock") {
-      userSelection = "Rock";
-  } else if (userPlay.toLowerCase() == "paper"){
-    userSelection = "Paper";
-}
-    else {
-    userSelection = "Scissors"
-    }
-    
+
   console.log(`Computer plays ${computerSelection}`)
+
+  //Promts the user for input, stores it in the userSelection variable
+function playRound() {
+let userPlay = prompt("Rock, Paper, or Scissors?");
+
+
+if (userPlay.toLowerCase() == "rock") {
+    userSelection = "Rock";
+} else if (userPlay.toLowerCase() == "paper"){
+  userSelection = "Paper";
+}
+  else {
+  userSelection = "Scissors"
+  }
+
+  //Displays User Choice
   console.log(`User Plays ${userSelection}`)
 
-  //Define the rules
-function playRound(userSelection, computerSelection){
+  //Defines the rules
     if (userSelection == "Rock" && computerSelection == "Paper") {
       return `${youLose}, Paper beats Rock`;
     }
@@ -57,6 +64,41 @@ function playRound(userSelection, computerSelection){
   }
 }
 
-//Play a Round, output winner
-winStatus = playRound(userSelection, computerSelection)
+
+//Plays a round, assigns winStatus to one of the three defined variables, tallies who won the round,
+function game() {
+    
+    winStatus = playRound()  
+    
+
+   if (winStatus === `${youWin}, Paper beats Rock`){
+        userScore = (userScore + 1);
+    }
+    else if (winStatus === `${youWin}, Rock beats Scissors`){
+        userScore = (userScore + 1);
+     }
+    else if (winStatus === `${youWin}, Scissors beat Rock`){
+        userScore = (userScore + 1);
+    }
+    else if (winStatus === `${youLose}, Scissors beat Paper`) {
+        computerScore = (computerScore + 1);
+    }
+    else if (winStatus === `${youLose}, Paper beats Rock`) {
+        computerScore = (computerScore + 1);
+    }
+    else if (winStatus === `${youLose}, Rock beats Scissors`) {
+        computerScore = (computerScore + 1);
+    }
+    else {
+        computerScore = computerScore, userScore = userScore;
+    }
+}
+
+game()
 console.log(winStatus)
+console.log(userScore)
+console.log(computerScore)
+
+
+
+
