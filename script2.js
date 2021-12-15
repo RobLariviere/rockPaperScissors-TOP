03e340edbd22b98f2931e5d83a038f5d7ed34c3d
RobLariviere <roblariviere18@gmail.com>
2021-12-15 12:50:11 -0500

    re-did the round function, got it working, copied 5 times and then checked for a winner

--- script2.js
+++ script2.js
@@ -5,12 +5,15 @@ let winStatus;
 let youTie = "You Tie, play again";
 let userSelection;
 let computerSelection;
-let userScore = 0
-let computerScore = 0
+let userScore = 0;
+let computerScore = 0;
+let resultMessage;
 
+function game() {
 
+function playRound() {
 
-//Define Computer's Play, Display it
+//Define Computer's Play
 computerPlay = Math.floor(Math.random() * 3) + 1;
 
 if (computerPlay == 1) {
@@ -21,10 +24,8 @@ if (computerPlay == 1) {
     computerSelection = "Scissors";
   }
 
-  console.log(`Computer plays ${computerSelection}`)
+//Promts the user for input, stores it in the userSelection variable, plays a
 
-  //Promts the user for input, stores it in the userSelection variable
-function playRound() {
 let userPlay = prompt("Rock, Paper, or Scissors?");
 
 
@@ -39,66 +40,78 @@ if (userPlay.toLowerCase() == "rock") {
 
   //Displays User Choice
   console.log(`User Plays ${userSelection}`)
+  console.log(`Computer plays ${computerSelection}`)
 
   //Defines the rules
-    if (userSelection == "Rock" && computerSelection == "Paper") {
-      return `${youLose}, Paper beats Rock`;
+   
+      
+      if (userSelection == "Rock" && computerSelection == "Paper") {
+      resultMessage = ("You lose, Paper beats Rock");
+      return winStatus = youLose, 
+      computerScore = computerScore + 1;
     }
     else if (userSelection == "Paper" && computerSelection == "Scissors") {
-      return `${youLose}, Scissors beat Paper`;
+      resultMessage = "You lose, Scissors beat Paper"
+      return winStatus= youLose, 
+      computerScore = computerScore + 1;
     }
     else if (userSelection == "Scissors" && computerSelection == "Rock"){
-    return `${youLose}, Rock beats Scissors`;
+      resultMessage = "You lose, Rock Beats Scissors"
+      return winStatus = youLose, 
+      computerScore = computerScore + 1;
     }
     else if (userSelection == "Paper" && computerSelection == "Rock"){
-      return `${youWin}, Paper beats Rock`;
+      resultMessage = "You win, Rock beats Paper"
+      return winStatus=youWin, 
+      userScore = userScore + 1;
     }
     else if (userSelection == "Scissors" && computerSelection == "Paper"){
-      return `${youWin}, Scissors beat Paper`;
+      resultMessage = "You win, Scissors beat paper"
+      return winStatus=youWin, 
+      userScore = userScore + 1;
     }
     else if (userSelection == "Rock" && computerSelection == "Scissors"){  
-      return `${youWin}, Rock beats Scissors`;
+    return winStatus=youWin, 
+    resultMessage = "You win, Rock beats Scissors",
+    userScore = userScore + 1;
     }
   else { 
+    resultMessage = "You Tie, Play Again"
       return youTie;
+    }
   }
-}
-
-
-//Plays a round, assigns winStatus to one of the three defined variables, tallies who won the round,
-function game() {
-    
-    winStatus = playRound()  
-    
-
-   if (winStatus === `${youWin}, Paper beats Rock`){
-        userScore = (userScore + 1);
-    }
-    else if (winStatus === `${youWin}, Rock beats Scissors`){
-        userScore = (userScore + 1);
-     }
-    else if (winStatus === `${youWin}, Scissors beat Rock`){
-        userScore = (userScore + 1);
-    }
-    else if (winStatus === `${youLose}, Scissors beat Paper`) {
-        computerScore = (computerScore + 1);
-    }
-    else if (winStatus === `${youLose}, Paper beats Rock`) {
-        computerScore = (computerScore + 1);
-    }
-    else if (winStatus === `${youLose}, Rock beats Scissors`) {
-        computerScore = (computerScore + 1);
-    }
-    else {
-        computerScore = computerScore, userScore = userScore;
-    }
-}
-
-game()
-console.log(winStatus)
+  //calls the game function, which calls the round function 5 times
+  
+playRound()
+console.log(resultMessage)
 console.log(userScore)
 console.log(computerScore)
+playRound()
+console.log(resultMessage)
+console.log(userScore)
+console.log(computerScore)
+playRound()
+console.log(resultMessage)
+console.log(userScore)
+console.log(computerScore)
+playRound()
+console.log(resultMessage)
+console.log(userScore)
+console.log(computerScore)
+playRound()
+console.log(resultMessage)
+console.log(userScore)
+console.log(computerScore)
+}
+game()
 
-
+//displays end message, win, lose or draw
+if (userScore > computerScore) {
+  console.log("You win the Match!")
+}
+else if (userScore < computerScore) {
+  console.log("You LOSE!!!")
+}
+else console.log("You Tie, Go again")
 
 
